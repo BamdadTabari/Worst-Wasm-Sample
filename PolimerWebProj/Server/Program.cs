55d99baec9c5ac85b088b45d4365f63da4adc004
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-04-18 06:27:50 -0700

    .

--- PolimerWebProj/Server/Program.cs
+++ PolimerWebProj/Server/Program.cs
@@ -1,11 +1,52 @@
+using KernelLogic.DataBaseObjects;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using PolimerWebProj.Shared.BasicObjects.JWT;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    //options.UseSqlServer("Server=faragos2_data; Initial Catalog=polimer;Integrated Security=false;User Id=faragos2_illegible; Password=abdalbaliISnothere@%$%");
 
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:MainData"]).EnableDetailedErrors().UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+});
+builder.Services.AddDbContext<IDentityContext>(options =>
+{
+    //options.UseSqlServer("Server=faragos2_data; Initial Catalog=polimer;Integrated Security=false;User Id=faragos2_illegible; Password=abdalbaliISnothere@%$%");
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:MainData"]).EnableDetailedErrors().UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IDentityContext>().AddDefaultTokenProviders();
+
+#region IDentity with jwt
+
+var jwtSetting = new JwtSetting();
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = jwtSetting.ValidateIssuer,
+        ValidateAudience = jwtSetting.ValidateAudience,
+        ValidateLifetime = jwtSetting.ValidateLifetime,
+        ValidateIssuerSigningKey = jwtSetting.ValidateIssuerSigningKey,
+        ValidIssuer = jwtSetting.ValidIssuer,
+        ValidAudience = jwtSetting.ValidAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSetting.SecuritySignInKey)),
+    };
+});
+
+#endregion
 
 var app = builder.Build();
 
@@ -28,7 +69,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
---
+++ PolimerWebProj/Shared/BasicObjects/JWT/JwtSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimerWebProj.Shared.BasicObjects.JWT
+{
+    public class JwtSetting
+    {
+        public bool ValidateIssuerSigningKey { get; set; } = true;
+        public bool ValidateLifetime { get; set; } = true;
+        public bool ValidateAudience { get; set; } = true;
+        public bool ValidateIssuer { get; set; } = true;
+        public string ValidIssuer { get; set; } = "https://localhost";
+        public string ValidAudience { get; set; } = "https://localhost";
+        public string SecuritySignInKey { get; set; } = "WQ7+dPhLEHdhdaKNzu!ck-fg86TPhUfd#E&&Qq+=vUtfxJ!@sDfe#u^prXW2&Qhmy33u!@e?5-xb*";
+        public int ExpiryInMinutes { get; set; } = 1024;
+    }
+}
